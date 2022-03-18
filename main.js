b19586fe949207a6dc31a664702998a8e6270e2f
Dennis Koch <denniskoch@Denniss-Air.fritz.box>
2022-03-18 12:34:26 +0100

    Full Project

---
+++ main.js
@@ -0,0 +1,15 @@
+function charCount(str) {
+  let lng = str.length;
+  document.getElementById("charCount").innerHTML = lng;
+}
+
+function charCountAnswer(str) {
+  let lng = str.length;
+  document.getElementById("charCountTwo").innerHTML = lng;
+}
+
+function charCountTags(str) {
+  let lng = str.length;
+  document.getElementById("charCountThree").innerHTML = lng;
+}
+
---
+++ readme.md
@@ -0,0 +1,8 @@
+<body>
+<h2>Question:</h2>
+<textarea></textarea>
+<h2>Answer:</h2>
+<textarea></textarea>
+<h2>Tags, comma seperated:</h2>
+<textarea></textarea>    
+</body>
